fe84b0498281520a87ed921d18c0b73a26248d4f
Yousefatt <yousefyaser852@gmail.com>
2024-10-13 00:00:31 +0300

    this is

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+def canUnlockAll(boxes):
+
+    print(boxes)
+
+    print("boxs: ", boxes)
+    print("The total of boxes: ", len(boxes))
+    setofkeys = []
+    counter = 0
+    Total_boxes = len(boxes)
+    for key in boxes[0]: 
+        if key < Total_boxes and key not in setofkeys and key > 0:
+            setofkeys.append(key)
+            counter +=1
+    index = 0
+    while index < len(setofkeys):
+        setkey = setofkeys[index]
+        setkey = setofkeys[index]
+        for key in boxes[setkey]:
+            if key < Total_boxes and key not in setofkeys and key > 0:
+                setofkeys.append(key)
+                counter +=1
+        index += 1
+    print("Total keys: ", counter)
+    if (counter == Total_boxes-1):
+        return True 
+    else: 
+        return False
